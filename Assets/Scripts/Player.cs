f4d9cb2cd09dd0fa995ecb42fc5197562e31f3b0
Dylan O’Connor <dylanoconnor@Dylans-MacBook-Pro.local>
2024-10-28 08:49:23 -0700

    snow tessalation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public float speed = 5.0f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Calculate movement direction based on WASD input
+        float moveHorizontal = Input.GetAxis("Horizontal"); // A/D or Left/Right Arrow Keys
+        float moveVertical = Input.GetAxis("Vertical");     // W/S or Up/Down Arrow Keys
+
+        // Create a movement vector
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // Move the object in the calculated direction, scaled by speed and deltaTime
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
     }
 }
