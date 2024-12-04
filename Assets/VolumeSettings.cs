82b800ed5f97d7f585ae6cf797483fc4eb4f21c6
keelinr20 <keelinrobbins@gmail.com>
2024-12-04 11:00:45 +0100

    adding active scenes

---
+++ Assets/VolumeSettings.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class VolumeSettings : MonoBehaviour
+{
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+    }
+}
