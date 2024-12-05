9526cc5adafcfa986bb77532badd7d106d521a57
Ian Kim <iank0426@gmail.com>
2024-12-05 15:54:42 +0100

    Levels Page Stuff

--- Assets/Scripts/LevelPage.cs
+++ Assets/Scripts/LevelPage.cs
@@ -3,7 +3,6 @@ using UnityEngine.SceneManagement;
 
 public class LevelPage : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
@@ -11,10 +10,21 @@ public class LevelPage : MonoBehaviour
             LoadScene();
         }
     }
-
-    // Update is called once per frame
     void LoadScene()
     {
-        SceneManager.LoadScene("Levels Page");
+        if (SceneManager.GetActiveScene().name == "FrostyFrolic")
+        {
+            SceneManager.LoadScene("MerryMaze");
+        }
+
+        if (SceneManager.GetActiveScene().name == "MerryMaze")
+        {
+            SceneManager.LoadScene("PolarPlatforms");
+        }
+
+        if (SceneManager.GetActiveScene().name == "PolarPlatforms")
+        {
+            SceneManager.LoadScene("EndScene");
+        }
     }
 }
