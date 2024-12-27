5cdcece49c7cef69054cfc12e29e835b9757e8d1
kwm26 <kwm26@duke.edu>
2024-12-26 17:03:06 -0700

    p

--- Assets/Scripts/Audio/BackgroundMusic.cs
+++ Assets/Scripts/Audio/BackgroundMusic.cs
@@ -1,16 +1,23 @@
-using System;
-using UnityEngine;
-using FMOD.Studio;
 using FMODUnity;
+using FMOD.Studio;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackgroundMusic : MonoBehaviour
 {
-    [SerializeField] private EventReference musicEvent; // Assign the FMOD music event in the Inspector
+    [SerializeField] private EventReference musicEvent; 
     private EventInstance musicInstance;
+    private static bool musicStarted = false; // Tracks if music has already started
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject);
+
+        
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            ResetMusic();
+        }
     }
 
     private void Start()
@@ -21,8 +28,12 @@ public class BackgroundMusic : MonoBehaviour
             return;
         }
 
-        musicInstance = RuntimeManager.CreateInstance(musicEvent);
-        musicInstance.start();
+        if (!musicStarted)
+        {
+            musicInstance = RuntimeManager.CreateInstance(musicEvent);
+            musicInstance.start();
+            musicStarted = true;
+        }
     }
 
     private void OnDestroy()
@@ -33,5 +44,16 @@ public class BackgroundMusic : MonoBehaviour
             musicInstance.release();
         }
     }
+
+    private void ResetMusic()
+    {
+        if (musicInstance.isValid())
+        {
+            musicInstance.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+            musicInstance.release();
+            musicStarted = false;
+        }
+    }
 }
+
 
